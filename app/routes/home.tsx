2dc62d8638afb612887075fdd296c7ed9849aeda
Aadesh Pokhrel <aadeshpkrel@gmail.com>
2025-12-22 12:20:32 +0545

    Homepage, Navbar & ResumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import {resume} from "react-dom/server";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVmind" },
+    { name: "description", content: "Your smart way to make CV!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Keep Record of Your Application & CV ratings</h1>
+        <h2>Review Your Cv with AI-powered Feedback</h2>
+      </div>
+
+
+        {resumes.length > 0 && (
+            <div className="resumes-section">
+                {resumes.map((resume) => (
+                    <ResumeCard key={resume.id} resume={resume}/>
+                ))}
+            </div>
+        )}
+    </section>
+
+
+
+
+    </main>
 }
